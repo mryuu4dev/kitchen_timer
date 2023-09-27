928708036f5596da251375cb313f5294b9f5fd3f
Yuuto Harada <mryuu.dev@gmail.com>
2023-09-27 09:54:29 +0900

    カウントアップタイマーを実装

--- lib/main.dart
+++ lib/main.dart
@@ -1,68 +1,80 @@
-import 'package:flutter/material.dart';
+import 'dart:async';
 
-void main() {
-  runApp(const MyApp());
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+void main() => runApp(MyApp());
+
+class StopwatchController extends GetxController {
+  late final Timer _timer;
+  final Stopwatch _stopwatch = Stopwatch();
+
+  final RxString elapsedTime = '00:00'.obs;
+
+  @override
+  void onInit() {
+    _timer = Timer.periodic(const Duration(milliseconds: 30), (timer) {
+      int elapsedMillis = _stopwatch.elapsedMilliseconds;
+      int seconds = (elapsedMillis ~/ 1000) % 60;
+      int millisec = (elapsedMillis % 1000) ~/ 10;
+
+      elapsedTime.value = '${_padZero(seconds)}:${_padZero(millisec)}';
+    });
+    super.onInit();
+  }
+
+  @override
+  void dispose() {
+    _timer.cancel();
+    super.dispose();
+  }
+
+  void startStopTimer() {
+    if (_stopwatch.isRunning) {
+      _stopwatch.stop();
+    } else {
+      _stopwatch.start();
+    }
+  }
+
+  void resetTimer() {
+    _stopwatch.stop();
+    _stopwatch.reset();
+  }
+
+  String _padZero(int num) => num.toString().padLeft(2, '0');
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  final stopwatchController = Get.put(StopwatchController());
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        body: Center(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: <Widget>[
+              Obx(
+                () => Text(
+                  stopwatchController.elapsedTime.value,
+                  style: const TextStyle(fontSize: 50),
+                ),
+              ),
+              ElevatedButton(
+                onPressed: () => stopwatchController.startStopTimer(),
+                child: const Text('スタート/ストップ'),
+              ),
+              ElevatedButton(
+                onPressed: () => stopwatchController.resetTimer(),
+                child: const Text('リセット'),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
       ),
     );
   }
